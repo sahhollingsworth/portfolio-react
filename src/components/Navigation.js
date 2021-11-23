4963bb029f8e2c4666fa42071d54f6a34d5de3c1
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-22 18:45:56 -0800

    Created Navigation component with all sections of the page .
    When selection is selected or actively viewed by user, corresponding title in the navbar is visually highlighted

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
+function Navigation({ currentPage, handlePageChange }) {
+    return (
+        <Navbar>
+            <Container>
+                <Navbar.brand>SARAH HOLLINGSWORTH</Navbar.brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                        About Me
+                    </Nav.Link>
+                    <Nav.Link href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                        Portfolio
+                    </Nav.Link>
+                    <Nav.Link href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                        Resume
+                    </Nav.Link>
+                    <Nav.Link href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                        Contact
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
 
-
-// function Navigation() {
-//     return (
-//         <Navbar>
-//             <Container>
-//                 <Navbar.brand>SARAH HOLLINGSWORTH</Navbar.brand>
-//                 <Nav className="me-auto">
-//                     <Nav.Link href="#about">About Me</Nav.Link>
-//                     <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-//                     <Nav.Link href="#resume">Resume</Nav.Link>
-//                     <Nav.Link href="#contact">Contact</Nav.Link>
-//                 </Nav>
-//             </Container>
-//         </Navbar>
-//     );
-// }
-
-// export default Navigation;
+export default Navigation;
