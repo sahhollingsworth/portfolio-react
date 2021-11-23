98be7f9f041b103bc384618d2c59e0e96b85ff07
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 14:59:07 -0800

    Replaced Footer's brand Fontawesome icons with png's.
    Fix: Removed erroneous id from image tag in About.
    Feat: added brand icons to src/images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,36 @@
 import React from 'react';
+// Images for social media links
+import github from '../images/logo_github.png';
+import linkedIn from '../images/logo_linkedin.png';
+import medium from '../images/logo_ medium.png';
 
 function Footer() {
     return (
         <footer className="bg-dark text-center text-white">
-        <div className="container p-2">
-            {/* Links  */}
-            <section className="mb-2">
-                {/* Linkedin */}
-                <a className="btn btn-floating m-1" role="button" href="https://github.com/sahhollingsworth" target="_blank" rel="noreferrer">
-                    <i className="fab fa-linkedin-in"></i>
-                </a>
-                {/* Github  */}
-                <a className="btn btn-floating m-1" role="button" href="https://www.linkedin.com/in/sarahhollingsworth/" target="_blank" rel="noreferrer">
-                    <i className="fab fa-github"></i>
-                </a>
-                {/* Medium */}
-                <a className="btn btn-floating m-1" role="button" href="https://medium.com/@sarahholli" target="_blank" rel="noreferrer">
-                    <i className="fab fa-facebook-f"></i>
-                </a>
-            </section>
-            {/* end Links  */}
-            {/* Copyright */}
-            <div className="text-center text-muted">
-                <div>&copy Sarah Hollingsworth 2021</div>
+            <div className="container p-2">
+                {/* Links  */}
+                <section className="mb-2">
+                    {/* Linkedin */}
+                    <a className="btn btn-floating m-1" role="button" href="https://github.com/sahhollingsworth" target="_blank" rel="noreferrer">
+                        <img src={github} alt="Github logo" className="img-thumbnail"/>
+                    </a>
+                    {/* Github  */}
+                    <a className="btn btn-floating m-1" role="button" href="https://www.linkedin.com/in/sarahhollingsworth/" target="_blank" rel="noreferrer">
+                        <img src={linkedIn} alt="LinkedIn logo" className="img-thumbnail"/>
+                    </a>
+                    {/* Medium */}
+                    <a className="btn btn-floating m-1" role="button" href="https://medium.com/@sarahholli" target="_blank" rel="noreferrer">
+                        <img src={medium} alt="Medium blogsite logo" className="img-thumbnail"/>
+                    </a>
+                </section>
+                {/* end Links  */}
+                {/* Copyright */}
+                <div className="text-center text-muted">
+                    <div>&copy Sarah Hollingsworth 2021</div>
+                </div>
+                {/* end Copyright */}
             </div>
-            {/* end Copyright */}
-        </div>
-    </footer>
+        </footer>
     )
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
                 <h1 className="display-3">ABOUT</h1>
             </div>
             <div className="col-4">
-                <img src={headshot} alt="Sarah Hollingsworth" className="img-thumbnail" id="img-me" />
+                <img src={headshot} alt="Sarah Hollingsworth" className="img-thumbnail" />
             </div>
             <div className="col-6">
                 <p className="px-2">Soon-to-be web developer with a background in consumer support, operations, and product management for web and mobile apps. I enjoy approaching application developement from a wholelistic perspective, prioritizing user experience, product market fix, as well as low impact and sustainable infrastructure for growth. This an exploration of React to make a simple portfolio, but stayed tuned for a new portfolio soon!.
