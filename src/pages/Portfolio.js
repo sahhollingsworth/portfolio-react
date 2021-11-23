e533c4743d0dfdf30ca5d14be655a277b987ed1a
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 15:29:53 -0800

    Added placeholder text to Contact and Portfolio pages to resolve load issue

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <p>portfolio placeholder text</p>
+        </div>
+    );
+
+}
+
+export default Portfolio;
