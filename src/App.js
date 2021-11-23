38f0544611580df6e22ed09e4e643da6de4f5ac0
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-22 18:15:47 -0800

    Fix: App was a function instead of a variable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import './App.css';
 
-function App() {
-  return (
-    <Header />
-  );
-}
+const App = () => <Header />;
 
 export default App;
