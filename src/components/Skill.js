ce663696bbff7aae9f36fff44144d632a03a40a9
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 15:28:12 -0800

    Added skill constants for use in Resume page

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,7 +1,5 @@
 // import React from 'react';
 
-// //create skills to pass through to skill function?
-
 // function Skill() {
 
 // }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,95 @@
 import React from 'react';
-import resumePDF from '../images/sarah_hollingsworth_resume_2021-11.pdf.pdf'; 
-import Skill from '../components/Skill';
+// import resumePDF from '../images/sarah_hollingsworth_resume_2021-11.pdf.pdf'; 
+// import Skill from '../components/Skill';
 
 // create skills to pass through to skill function?
 
+// // Languages, Libraries, and Packages
+// const languages = [
+//     {
+//         name: "JQuery"
+//     },
+//     {
+//         name: "GraphQL"
+//     },
+//     {
+//         name: "JWT"
+//     },
+//     {
+//         name: "Express"
+//     },
+//     {
+//         name: "Apollo Client"
+//     },
+//     {
+//         name: "Javascript"
+//     },
+//     {
+//         name: "JSX"
+//     },
+//     {
+//         name: "Node"
+//     },
+//     {
+//         name: "React"
+//     },
+//     {
+//         name: "JSON"
+//     },
+//     {
+//         name: "HTML"
+//     },
+//     {
+//         name: "CSS"
+//     },
+// ]
+
+// const technologies = [
+//     {
+//         name= "Progressive Web Apps"
+//     },
+//     {
+//         name= "Heroku"
+//     },
+//     {
+//         name= "MongoDB Atlas"
+//     },
+//     {
+//         name= "GitHub"
+//     },
+//     {
+//         name: "Bootstrap"
+//     },
+//     {
+//         name: "Bulma"
+//     },
+//     {
+//         name: "REST APIs"
+//     },
+//     {
+//         name: "Insomnia"
+//     }
+// ]
+
+// // Database & Database related
+// const databases = [
+//     {
+//         name: "MongoDB"
+//     },
+//     {
+//         name: "MySQL"
+//     },
+//     {
+//         name: "Indexed Database API"
+//     }
+// ]
+
 function Resume() {
+    return (
+        <div>
+            <p>Resume placeholder text</p>
+        </div>
+    );
 
 }
 
