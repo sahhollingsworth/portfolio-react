384e574dc7ab8e3b2d58717e2721ba07b5ad16f2
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 19:41:40 -0800

    Feat: Function to validate email address format in contact form using Regex

---
+++ src/utils/formValidation.js
@@ -0,0 +1,6 @@
+export function validateEmail(email) {
+    // Regex that matches most standard email formats
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Checks the now lower case user input for email to see if it matches the regular expression for email formats
+    return regexEmail.test(String(email).toLowerCase());
+}
