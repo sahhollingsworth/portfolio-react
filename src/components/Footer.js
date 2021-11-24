edc3763a7ae41b991cffa29cfae0602320fd8b4e
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 17:05:20 -0800

    Replace FontAwesome icon & ino.png usage with React-Icons package
    Removed icons from images dir.
    removed fontawesome link from index.html
    replaced use of icon images with react-icon in Footer.js
    Added Footer.css for custom coloring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,41 @@
 import React from 'react';
-// Images for social media links
-import github from '../images/logo_github.png';
-import linkedIn from '../images/logo_linkedin.png';
-import medium from '../images/logo_medium.png';
+// import Icons
+import { IconContext } from "react-icons";
+import { BsGithub } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaMedium } from 'react-icons/fa';
+
+import './Footer.css';
+
 
 function Footer() {
     return (
-        <footer className="bg-dark text-center text-white">
+        <footer className="text-center text-white">
             <div className="container p-2">
                 {/* Links  */}
                 <section className="mb-2">
-                    {/* Linkedin */}
-                    <a className="btn btn-floating m-1" role="button" href="https://github.com/sahhollingsworth" target="_blank" rel="noreferrer">
-                        <img src={github} alt="Github logo" className="img-thumbnail"/>
-                    </a>
-                    {/* Github  */}
-                    <a className="btn btn-floating m-1" role="button" href="https://www.linkedin.com/in/sarahhollingsworth/" target="_blank" rel="noreferrer">
-                        <img src={linkedIn} alt="LinkedIn logo" className="img-thumbnail"/>
-                    </a>
-                    {/* Medium */}
-                    <a className="btn btn-floating m-1" role="button" href="https://medium.com/@sarahholli" target="_blank" rel="noreferrer">
-                        <img src={medium} alt="Medium blogsite logo" className="img-thumbnail"/>
-                    </a>
+                    <IconContext.Provider value={{ style: {height: "2em", width: "2em"}, className: "global-class-name" }}>
+                        {/* Linkedin */}
+                        <a className="btn btn-floating m-1" role="button" href="https://www.linkedin.com/in/sarahhollingsworth/" target="_blank" rel="noreferrer">
+                            {/* <img src={linkedIn} alt="LinkedIn logo" className="img-thumbnail"/> */}
+                            <FaLinkedin />
+                        </a>
+                        {/* Github  */}
+                        <a className="btn btn-floating m-1" role="button" href="https://github.com/sahhollingsworth" target="_blank" rel="noreferrer">
+                            {/* <img src={github} alt="Github logo" className="img-thumbnail"/> */}
+                            <BsGithub />
+                        </a>
+                        {/* Medium */}
+                        <a className="btn btn-floating m-1" role="button" href="https://medium.com/@sarahholli" target="_blank" rel="noreferrer">
+                            {/* <img src={medium} alt="Medium blogsite logo" className="img-thumbnail"/> */}
+                            <FaMedium />
+                        </a>
+                    </IconContext.Provider>
                 </section>
                 {/* end Links  */}
                 {/* Copyright */}
-                <div className="text-center text-muted">
-                    <div>&copy Sarah Hollingsworth 2021</div>
+                <div className="text-center">
+                    <div>&copy; Sarah Hollingsworth 2021</div>
                 </div>
                 {/* end Copyright */}
             </div>
@@ -35,4 +44,3 @@ function Footer() {
 }
 
 export default Footer;
-
