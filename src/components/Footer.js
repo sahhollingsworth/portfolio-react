e9bbf1ce6672582ce52db04b562f5b723a5ec40b
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 20:48:27 -0800

    Fix: Removed extra spacing from Footer that made it appear off y center
    Style: added a Container element to About page to standardize spacing.
    Fix: Removed unneccessary section title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     return (
         <footer className="text-center text-white">
             <div className="container p-2">
-                <section className="mb-2">
+                <section className="pt-1">
                     <IconContext.Provider value={{ style: {height: "2em", width: "2em"}, className: "global-class-name" }}>
                         {/* Linkedin */}
                         <a className="btn btn-floating m-1" role="button" href="https://www.linkedin.com/in/sarahhollingsworth/" target="_blank" rel="noreferrer">
