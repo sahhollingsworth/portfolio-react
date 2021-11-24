8abd5ef08f0498c5b896f4c0529a80d7d7cd1135
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-23 19:58:08 -0800

    Feat: Function to validate that a user has entered at least 1 character in each form input

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,6 +1,12 @@
+// Validation function to check that email address is an actual email address
 export function validateEmail(email) {
     // Regex that matches most standard email formats
     const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     // Checks the now lower case user input for email to see if it matches the regular expression for email formats
     return regexEmail.test(String(email).toLowerCase());
+}
+
+// Validation that user provides data in each contact form input
+export function completeField(inputData) {
+    return(inputData.length > 0 ? true : false)
 }
